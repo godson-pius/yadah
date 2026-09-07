bcdb2e2d97614f589c42c4376813ab4115bfd936
godson-pius <godsonazubuike15@gmail.com>
2026-09-07 22:52:41 +0200

    Comment out unused Resend import in email route

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer";
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
+// import { Resend } from "resend";
 
 // const resend = new Resend(process.env.RESEND_API_KEY || "");
 
